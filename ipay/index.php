12a75160b62a9294f2dd8eedbde475625c8f9df4
rmramos23 <rmramos2399@gmail.com>
2025-11-11 09:43:02 +0800

    office pc

--- ipay/index.php
+++ ipay/index.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=`device-width`, initial-scale=1.0">
-    <title>Document</title>
+   
+    <link rel="stylesheet" href="..\resources/css/index.css">
+    <script src="..\resources/javascript/index.js"></script>
+    
+
+    <title>Login</title>
 </head>
+
+
+
 <body>
+
+<div class="container">
+    <div class="title">
+        <h1>MCA Solutions Limited</h1>
+        <h2>(iPay System)</h2>
+    </div>
+    <div class="userIdTitle">
+        <h3>User ID:</h3>
+    </div>
+    <div class="userIdInput">
+        <input type="text" class="userIdInputBox">
+    </div>
+    <div class="passwordTitle">
+        <h3>Password:</h3>
+    </div>
+    <div class="passwordInput">
+        <input type="text" class="passwordInputBox">
+    </div>
+    <div class="loginButton">
+        <input type="button" value="Login" class="loginButtonButton">
+    </div>
+</div>
 
 </body>
 </html>
