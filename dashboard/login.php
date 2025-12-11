2b5f1d0596027cbda42eca9058d1321f33285ef8
rmramos23 <rmramos2399@gmail.com>
2025-12-11 17:57:08 +0800

    payrollTimekeeping generate
    not yet done

--- dashboard/login.php
+++ dashboard/login.php
@@ -12,7 +12,7 @@ session_start();
 
         $query = "select * from employee where userName = '$userName' limit 1";
         $result = mysqli_query($con, $query);
-        if($result)
+        if($result && mysqli_num_rows($result) > 0)
         {
             $user_data = mysqli_fetch_assoc($result);
 
